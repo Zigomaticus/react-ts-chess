ad09b436f6f34e8ecc8a374559c2b44573f22db5
denis <permwed@gmail.com>
2022-05-16 12:08:12 +0500

    create board and cells

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+// Components
+import BoardComponent from "./components/BoardComponent";
+// Models
+import { Board } from "./models/Board";
 
 function App() {
-  return <div className="App">App</div>;
+  const [board, setBoard] = useState(new Board());
+
+  useEffect(() => {
+    restart();
+  }, []);
+
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initCells();
+    setBoard(newBoard);
+  }
+
+  return (
+    <div className="App">
+      <BoardComponent board={board} setBoard={setBoard} />
+    </div>
+  );
 }
 
 export default App;
